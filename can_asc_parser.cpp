3553269b8013f1a8bab214302542299626a48c7f
nrisius-isu <nwrisius@iastate.edu>
2022-12-08 23:06:52 -0600

    Added functionality to write processed data to csv files by message.

--- can_asc_parser.cpp
+++ can_asc_parser.cpp
@@ -8,6 +8,8 @@
 #include <fstream>
 #include <cmath>
 #include <limits>
+#include <sys/stat.h>
+#include <string.h>
 
 typedef std::numeric_limits<double> dbl;
 
@@ -76,6 +78,10 @@ public:
     std::cout << time << " " << channel << " " << id << " " << length << " " << byte_0 << " " << byte_1 << " " << byte_2 << " " << byte_3 << " " << byte_4 << " " << byte_5 << " " << byte_6 << " " << byte_7<< std::endl;  
   }
 
+  std::string get_time(){
+    return time;
+  }
+
   std::string get_pgn(){
     return id.substr(2,4);
   }
@@ -117,6 +123,7 @@ public:
 
 class gps_lat_long{
 private:
+  std::string time;
   double latitude, longitude;
 
 public:
@@ -125,8 +132,8 @@ public:
   }
 
   void parse_data(can_message c){
-    int lat0, lat1, lat2, lat3, lat_offset;
-    int long0, long1, long2, long3, long_offset;
+    int lat0, lat1, lat2, lat3;
+    int long0, long1, long2, long3;
 
     //long long int lat_raw, long_raw;
 
@@ -137,6 +144,7 @@ public:
     
     //std::cout << lat0 << " " << lat1  << " " << lat2 << " " << lat3 <<  std::endl;
 
+    time = c.get_time();
     latitude = ((lat0 + (lat1 * pow(2,8)) + (lat2 * pow(2,16)) + (lat3 * pow(2,24))) * lat_scale ) -210;
     longitude = ((long0 + (long1 * pow(2,8)) + (long2 * pow(2,16)) + (long3 * pow(2,24))) * long_scale ) -210;
 
@@ -155,11 +163,20 @@ public:
     std::cout.precision(dbl::max_digits10);
     std::cout << "Latitude: " << std::fixed << latitude << " Longitude: " << std::fixed << longitude << std::endl;
   }
+
+  std::string csv_out(){
+    std::ostringstream line_out;
+
+    line_out << time << "," << std::fixed << latitude << "," << std::fixed << longitude;
+
+    return line_out.str();
+  }
   
 };
 
 class harvest_pinball_sensor{
 private:
+  std::string time;
   int wand_0l, wand_0r, wand_1l, wand_1r;
 
 public:
@@ -175,6 +192,7 @@ public:
     w2 = std::stoi(c.get_b4(), nullptr, 16); w3 = std::stoi(c.get_b5(), nullptr, 16);
     w4 = std::stoi(c.get_b6(), nullptr, 16); w5 = std::stoi(c.get_b7(), nullptr, 16);
 
+    time = c.get_time();
     wand_0l = w0 + ((w1 & 15) * pow(2,8));
     wand_0r = (w2 * pow(2,4)) + ((w1 & 240) >> 4);
     wand_1l = w3 + ((w4 & 15) * pow(2,8));
@@ -215,11 +233,20 @@ public:
     std::cout.precision(dbl::max_digits10);
     std::cout << "Wand-0: " << get_wand_0_sum() << " Wand-1: " << get_wand_1_sum() << std::endl;
   }
+
+  std::string csv_out(){
+    std::ostringstream line_out;
+
+    line_out << time << "," << wand_0r << "," << wand_0l << "," << get_wand_0_sum() << "," << wand_1r << "," << wand_1l << "," << get_wand_1_sum();
+
+    return line_out.str();
+  }
   
 };
 
 class gps_speed_dir{
 private:
+  std::string time;
   double speed, direction, pitch, altitude;
 
 public:
@@ -241,6 +268,7 @@ public:
     p0 = std::stoi(c.get_b4(), nullptr, 16); p1 = std::stoi(c.get_b5(), nullptr, 16);
     alt0 = std::stoi(c.get_b6(), nullptr, 16); alt1 = std::stoi(c.get_b7(), nullptr, 16);
 
+    time = c.get_time();
     speed = (sp0 + (sp1 * pow(2,8))) * kmh_scale * k_to_mi; 
     direction = (dir0 + (dir1 * pow(2,8))) * deg_scale;
     pitch = ((p0 + (p1 * pow(2,8))) * deg_scale) - 200;
@@ -269,6 +297,14 @@ public:
     std::cout.precision(dbl::max_digits10);
     std::cout << "Speed: " << std::fixed << speed << " Direction: " << std::fixed << direction << std::endl;
   }
+
+  std::string csv_out(){
+    std::ostringstream line_out;
+
+    line_out << time << "," << std::fixed << speed << "," << std::fixed << direction << "," << std::fixed << pitch << "," << std::fixed << altitude;
+
+    return line_out.str();
+  }
   
 };
   
@@ -289,8 +325,9 @@ int main(int argc, char *argv[]){
   int line_count = 0;
 
   std::string path_gen = "/home/student/Desktop/ComS327/project2";
+  std::string file_name = "Collection_1_2022-10-29_15-12-42-279297_Partition_0.asc";
   
-  i.open(path_gen + "/Collection 1 2022-10-29 15-12-42-279297 Partition 0.asc");   
+  i.open(path_gen + "/" + file_name);   
 
   if (i.is_open()){
     std::cout << "Success" << std::endl;
@@ -341,9 +378,9 @@ int main(int argc, char *argv[]){
     
   }
 
-  for (int x = 0; x < (int) p56.size(); x++){
-    p56[x]->print_message();
-  }
+  // for (int x = 0; x < (int) p56.size(); x++){
+  //   p56[x]->print_message();
+  // }
 
   //std::string t_1 = "4A";
   //std::cout << std::stoi(t_1, nullptr, 16) << " -> " << t_1 << std::endl;
@@ -365,12 +402,106 @@ int main(int argc, char *argv[]){
   std::cout << (int) p34.size() << std::endl;
   std::cout << (int) p56.size() << std::endl;
   std::cout << (int) p78.size() << std::endl;
+  std::cout << (int) gll.size() << std::endl;
 
+
+  std::string out_loc = "./";
+
+  std::string out_dir = out_loc + file_name.substr(0,(file_name.size()-4));
+  std::cout << out_dir << std::endl;
+
+  if (mkdir(out_dir.c_str(),0777) == 0){
+    std::cout << "SUCCESS AGAIN"  << std::endl;
+    std::cout << (int) gll.size()  << std::endl;
+    //if lat long data
+    if((int) gll.size() > 0){
+      //write file
+      std::string out_lat_long = "gps_lat_long.csv";
+      std::cout << out_lat_long << std::endl;
+      std::ofstream gll_file(out_dir + "/" + out_lat_long);
+      if(gll_file.is_open()){
+	std::cout << "GOT IT" << std::endl;
+      }
+      gll_file << "time,lat,long" << std::endl;
+      for (int j = 0; j < (int) gll.size(); j++){
+  	gll_file << gll[j]->csv_out() << std::endl;
+      }
+
+      gll_file.close();
+    }
+    //if speed dir data
+    if((int) gsd.size() > 0){
+      //write file
+      std::string out_speed_dir = "gps_speed_dir.csv";
+      std::cout << out_speed_dir << std::endl;
+      std::ofstream gsd_file(out_dir + "/" + out_speed_dir);
+      if(gsd_file.is_open()){
+	std::cout << "GOT IT" << std::endl;
+      }
+      gsd_file << "time,speed,direction,pitch,altitude" << std::endl;
+      for (int j = 0; j < (int) gsd.size(); j++){
+  	gsd_file << gsd[j]->csv_out() << std::endl;
+      }
+
+      gsd_file.close();
+    }
+    //if wand data
+    if((int) p12.size() > 0){
+      //write file
+      std::string out_pinball_12 = "harvest_pinball_row_1_2.csv";
+      std::cout << out_pinball_12 << std::endl;
+      std::ofstream p12_file(out_dir + "/" + out_pinball_12);
+      if(p12_file.is_open()){
+	std::cout << "GOT IT" << std::endl;
+      }
+      p12_file << "time, Row 1 - Left [counts],Row 1 - Right [counts],Row 1 - Sum [counts],Row 2 - Left [counts],Row 2 - Right [counts],Row 2 - Sum [counts]" << std::endl;
+      for (int j = 0; j < (int) p12.size(); j++){
+  	p12_file << p12[j]->csv_out() << std::endl;
+      }
+
+      p12_file.close();
+    }
+    if((int) p34.size() > 0){
+      //write file
+      std::string out_pinball_34 = "harvest_pinball_row_3_4.csv";
+      std::cout << out_pinball_34 << std::endl;
+      std::ofstream p34_file(out_dir + "/" + out_pinball_34);
+      if(p34_file.is_open()){
+	std::cout << "GOT IT" << std::endl;
+      }
+      p34_file << "time, Row 3 - Left [counts],Row 3 - Right [counts],Row 3 - Sum [counts],Row 4 - Left [counts],Row 4 - Right [counts],Row 4 - Sum [counts]" << std::endl;
+      for (int j = 0; j < (int) p34.size(); j++){
+  	p34_file << p34[j]->csv_out() << std::endl;
+      }
+
+      p34_file.close();
+    }
+    if((int) p56.size() > 0){
+      //write file
+      std::string out_pinball_56 = "harvest_pinball_row_5_6.csv";
+      std::cout << out_pinball_56 << std::endl;
+      std::ofstream p56_file(out_dir + "/" + out_pinball_56);
+      if(p56_file.is_open()){
+	std::cout << "GOT IT" << std::endl;
+      }
+      p56_file << "time, Row 5 - Left [counts],Row 5 - Right [counts],Row 5 - Sum [counts],Row 6 - Left [counts],Row 6 - Right [counts],Row 6 - Sum [counts]" << std::endl;
+      for (int j = 0; j < (int) p56.size(); j++){
+  	p56_file << p56[j]->csv_out() << std::endl;
+      }
+
+      p56_file.close();
+    }
+  }else {
+    std::cout << "Could not create directory...file may have already been parsed"  << std::endl;
+  }
+
+  //std::cout << gll[1]->csv_out() << std::endl;
+
+  
   gll.clear();
   gsd.clear();
   p12.clear();
   p34.clear();
   p56.clear();
   p78.clear();
-  
 }
